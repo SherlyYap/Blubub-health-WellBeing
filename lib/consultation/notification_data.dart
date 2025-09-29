3febeb2c4abc7268b5c69787dca49de0ab7aef2e
Sherly Yap <sherly.yap@students.mikroskil.ac.id>
2025-09-29 08:19:56 +0700

    perbaikan sedikit

--- lib/consultation/notification_data.dart
+++ lib/consultation/notification_data.dart
@@ -6,14 +6,12 @@ List<Map<String, String>> customNotifications = [];
 Future<void> saveNotification(String title, String message) async {
   final prefs = await SharedPreferences.getInstance();
 
-  // Tambahkan ke list runtime
   customNotifications.add({
     'title': title,
     'message': message,
     'time': DateTime.now().toIso8601String(),
   });
 
-  // Simpan ke SharedPreferences
   await prefs.setString('notifications', jsonEncode(customNotifications));
 }
 
@@ -21,7 +19,16 @@ Future<void> loadNotifications() async {
   final prefs = await SharedPreferences.getInstance();
   final String? stored = prefs.getString('notifications');
   if (stored != null) {
-    customNotifications = List<Map<String, String>>.from(jsonDecode(stored));
+    final List<dynamic> decoded = jsonDecode(stored);
+
+    customNotifications =
+        decoded.map((item) {
+          return Map<String, String>.from(
+            item.map(
+              (key, value) => MapEntry(key.toString(), value.toString()),
+            ),
+          );
+        }).toList();
   }
 }
 
