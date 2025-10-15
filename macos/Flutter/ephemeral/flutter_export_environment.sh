32468ce6af1ad1b49bb6a5606a7bb74c53efb146
VincentCTio <vincent.christian@students.mikroskil.ac.id>
2025-10-15 21:44:48 +0700

    menyesuaikan fungsi menghapus notifikasi
    membuat agar bisa menghapus semua notifikasi dengan menambahkan sebuah chip

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\Vincent C Tio\flutter\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Vincent C Tio\OneDrive\Dokumen\GitHub\Blubub-health-WellBeing"
+export "FLUTTER_APPLICATION_PATH=E:\Blubub-health-WellBeing"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
